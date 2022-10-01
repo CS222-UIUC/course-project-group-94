e2ea2facef5a9cfc6710a9ec302ddd4527800fb9
alleng2 <92739180+alleng2@users.noreply.github.com>
2022-09-30 19:43:42 -0500

    Added tests and modified function
    Modified files to fit with flake8. Added tests and tested locally.

---
+++ backend/image_to_string.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from PIL import Image
+import pytesseract
+
+'''
+Function to convert an image of a specified name to a string ("example.jpg").
+The string will be stored in a list so that it can be later modified by users
+as they wish and accessed by the function to read data from the string.
+'''
+
+
+def convertImageToString(imgName, dataList):
+    imgString = pytesseract.image_to_string(Image.open(imgName))
+    dataList.append(imgString)
+    return imgString
---
+++ backend/test_image_to_string.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import image_to_string
+import pytesseract as pyt
+
+pyt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+
+def testNumOfStrings():
+    listOfStrings = []
+    assert len(listOfStrings) == 0, "Incorrect initial size of list"
+    imageName = "Label.jpg"
+    image_to_string.convertImageToString(imageName, listOfStrings)
+    assert len(listOfStrings) == 1, "Incorrect size of list after adding " \
+        "one string"
+    imageName = "OtherLabel.png"
+    image_to_string.convertImageToString(imageName, listOfStrings)
+    assert len(listOfStrings) == 2, "Incorrect size of list after adding " \
+        "two strings"
+
+
+def testContentOfStrings():
+    newList = []
+    imageName = "eng_bw.png"
+    image_to_string.convertImageToString(imageName, newList)
+    imageName = "Label.jpg"
+    image_to_string.convertImageToString(imageName, newList)
+    assert newList[0][0:14] == "Mild Splendour", "Incorrect reading of " \
+        "example text"
+    assert newList[1][0:15] == "Nutrition Facts", "Incorrect reading of " \
+        "example label"
+    assert "Total Fat" in newList[1], "Missing from Middle of Label"
+    assert "Potassium" in newList[1], "Missing from End of Label"
+    assert len(newList) == 2, "Incorrect size of list"
