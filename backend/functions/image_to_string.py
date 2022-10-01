ce39593d1bc509687f867f39ae9f87f817f4752e
Allen Guo <alleng2@illinois.edu>
2022-09-30 20:16:05 -0500

    Modified to make more organized.
     Please enter the commit message for your changes. Lines starting

--- backend/functions/image_to_string.py
+++ backend/functions/image_to_string.py
@@ -3,10 +3,12 @@ from PIL import Image
 import pytesseract
 
 '''
-Function to convert an image of a specified name to a string ("example.jpg"). 
-The string will be stored in a list so that it can be later modified by users 
+Function to convert an image of a specified name to a string ("example.jpg").
+The string will be stored in a list so that it can be later modified by users
 as they wish and accessed by the function to read data from the string.
 '''
+
+
 def convertImageToString(imgName, dataList):
     imgString = pytesseract.image_to_string(Image.open(imgName))
     dataList.append(imgString)
