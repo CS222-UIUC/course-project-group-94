28824894c8cb44e76e00570bc44370ac1713ee1f
Justin Chen <jjchen10@illinois.edu>
2022-10-07 17:15:49 -0500

    Add basic UI with textboxes and button

---
+++ frontend/app/src/main/java/com/cs222/nutrify/ui/nutritioninput/NutritionInputActivity.kt
@@ -0,0 +1,13 @@
+package com.cs222.nutrify.ui.nutritioninput
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cs222.nutrify.R
+
+class NutritionInputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nutritioninput)
+    }
+}
