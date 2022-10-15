bde09ce0f1228da79dfdc0f26353d8613cbe7a44
maanavagrawal <maanav2@illinois.edu>
2022-10-14 23:44:02 -0500

    Completed the warning system for if a certain food exceeds or is below the user's preferences

--- backend/functions/selectprefs.py
+++ backend/functions/selectprefs.py
@@ -1,16 +1,36 @@
+from ftfy import warnings
 import sqlalchemy as db
 from sqlalchemy.sql import select
+import numpy as np
 
 #currently connecting to my local mySQL database (username: root, password: Group94)
-engine = db.create_engine('mysql://root:Group94@localhost:3306/Nutrify')
-connection = engine.connect()
-metadata = db.MetaData()
 
 # Working on gathering data from the database and 
 # comparing it to the diet preferences that the user inputted to see if it goes over
-nutritional_info.query(data).filter(data.nutritional_info.in_([user_preferences]))
-s = select([user_preferences])
-result = connection.execute(s)
-warning = False
-if result:
+def comparePreferences(calories, sugarDiet, biologicalSex, fat):
+    engine = db.create_engine('mysql://root:Group94@localhost:3306/Nutrify')
+    connection = engine.connect()
+    metadata = db.MetaData()
+
+    calories, sugarLowerRange, sugarUpperRange, lowerFat, upperFat = dietPreferenceReader(calories, sugarDiet, biologicalSex)
+    nutritional_info = db.Table("Running Total on Daily Nutrition", metadata, autoload = True, autoload_with = engine)
+    data = db.select([nutritional_info]).where(nutritional_info.columns.Username == username)
+    data = connection.execute(data).fetchall()
     warning = True
+    warningDict = {"Calories": warning, "Fat": warning, "Sugar": warning}
+    if (data[0][0] <= calories):
+        warningDict["Calories"] = True
+    else:
+        warningDict["Calories"] = False
+    if (data[0][1] >= lowerFat) and (data[0][1] <= upperFat):
+        warningDict["Fat"] = True
+    else:
+        warningDict["Fat"] = False
+    if (data[0][4] >= sugarLowerRange) and (data[0][4] <= sugarUpperRange):
+        warningDict["Sugar"] = True
+    else:
+        warningDict["Sugar"] = False
+
+    return warningDict
+
+
