8da78fef8f10d66ea01c7e55b2ee277480a034f4
Justin Chen <jjchen10@illinois.edu>
2022-10-21 21:48:49 -0500

    Add text listener and basic functionality to add food item button

---
+++ frontend/app/src/main/java/com/cs222/nutrify/viewmodels/NutritionInputViewModel.kt
@@ -0,0 +1,65 @@
+package com.cs222.nutrify.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.cs222.nutrify.models.NutritionInformation
+
+class NutritionInputViewModel : ViewModel() {
+
+    private val nutritionInformationList: MutableList<NutritionInformation> = mutableListOf()
+
+    val calories: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val fat: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val sodium: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val carbs: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val sugar: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val protein: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val calcium: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val iron: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val potassium: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                NutritionInputViewModel()
+            }
+        }
+    }
+
+    fun addNutritionInformation() {
+        val nutritionInformation = NutritionInformation()
+        nutritionInformation.calories = calories.value?.toDouble()
+        nutritionInformation.fat = fat.value?.toDouble()
+        nutritionInformation.sodium = sodium.value?.toDouble()
+        nutritionInformation.carbs = carbs.value?.toDouble()
+        nutritionInformation.sugar = sugar.value?.toDouble()
+        nutritionInformation.protein = protein.value?.toDouble()
+        nutritionInformation.calcium = calcium.value?.toDouble()
+        nutritionInformation.iron = iron.value?.toDouble()
+        nutritionInformation.potassium = potassium.value?.toDouble()
+        nutritionInformationList.add(nutritionInformation)
+    }
+
+    fun getNutritionInformationList(): List<NutritionInformation> = nutritionInformationList
+}
